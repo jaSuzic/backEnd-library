348f5067e47d31026d2d3403442dbaffe99aae31
Jaksa Suzic <jaksas@gmail.com>
2019-07-21 16:16:41 +0200

    rent finished, ready for auth

--- controllers/rent.js
+++ controllers/rent.js
@@ -1,82 +1,99 @@
-const Rent = require('../models/book')
+const Rent = require("../models/rent");
+const mongoose = require("mongoose");
 
 exports.getRents = (req, res, next) => {
-	const rentQuery = Rent.find();
-	let fetchedRents;
-	rentQuery.then(rents => {
-		fetchedRents = rents;
-		return Rent.countDocuments();
-	}).then(count => {
-		res.status(200).json({
-			message: 'Rents fetched successfully!',
-			rents: fetchedRents,
-			count: count
-		})
-	})
-}
+  const rentQuery = Rent.find()
+    .populate("bookId")
+    .populate("memberId");
+  let fetchedRents;
+  rentQuery
+    .then(rents => {
+      fetchedRents = rents;
+      return Rent.countDocuments();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Rents fetched successfully!",
+        rents: fetchedRents,
+        count: count
+      });
+    });
+};
 
 exports.setRent = (req, res, next) => {
-	const rent = new Rent({
-		memberId: req.body.memberId,
-		bookId: req.body.bookId,
-		rentDate: req.body.rentDate,
-		returnDate: req.body.returnDate
-	})
-	rent.save().then(result => {
-		res.status(201).json({
-			message: "New rent saved"
-		})
-	})
-}
+  console.log(req.body);
+  const rent = new Rent({
+    memberId: req.body.idMember,
+    bookId: req.body.idBook,
+    rentDate: req.body.rentDate,
+    returnDate: req.body.returnDate
+  });
+  console.log("Evo ga rent obj: ", rent);
+  rent.save().then(
+    result => {
+      res.status(201).json({
+        message: "New rent saved" + result
+      });
+    },
+    err => {
+      console.log(err);
+    }
+  );
+};
 
 //I think that there is not need for deleting rent, so maybe remove this method.
 exports.deleteRent = (req, res, next) => {
-	Rent.deleteOne({
-		_id: req.params.id
-	}).then(result => {
-		if (result.n > 0) {
-			res.status(201).json({
-				message: "Rent removed."
-			})
-		} else {
-			res.status(401).json({
-				message: "There was error, rent wasn't deleted."
-			})
-		}
-	})
-}
+  Rent.deleteOne({
+    _id: req.params.id
+  }).then(result => {
+    if (result.n > 0) {
+      res.status(201).json({
+        message: "Rent removed."
+      });
+    } else {
+      res.status(401).json({
+        message: "There was error, rent wasn't deleted."
+      });
+    }
+  });
+};
 
 exports.getRent = (req, res, next) => {
-	Rent.findById(req.params.id).then(rent => {
-		if (rent) {
-			res.status(200).json(rent)
-		} else {
-			res.status(404).json({
-				message: 'Rent not found'
-			})
-		}
-	})
-}
+  Rent.findById(req.params.id).then(rent => {
+    if (rent) {
+      res.status(200).json(rent);
+    } else {
+      res.status(404).json({
+        message: "Rent not found"
+      });
+    }
+  });
+};
 
 exports.updateRent = (req, res, next) => {
-	const rent = new Rent({
-		_id: req.params.id,
-		memberId: req.body.memberId,
-		bookId: req.body.bookId,
-		rentDate: req.body.rentDate,
-		returnDate: req.body.returnDate
-	})
-	Rent.updateOne({
-		_id: req.params.id
-	}, rent).then(result => {
-		if (result.n > 0) {
-			res.status(200).json({
-				message: "Updated successful"
-			})
-		} else {
-			res.status(401).json({
-				message: "Update failed"
-			})
-		}
-	})
-}
+  const rent = new Rent({
+    _id: req.params.id,
+    memberId: req.body.idMember,
+    bookId: req.body.idBook,
+    rentDate: req.body.rentDate,
+    returnDate: req.body.returnDate
+  });
+  console.log("U BE smo, evo ga rent: ", rent);
+  console.log("U BE smo, evo ga rent: ", req.body);
+  Rent.updateOne(
+    {
+      _id: req.params.id
+    },
+    rent
+  ).then(result => {
+    if (result.n > 0) {
+      res.status(200).json({
+        message: "Updated successful"
+      });
+    } else {
+      res.status(401).json({
+        message: "Update failed"
+      });
+    }
+  });
+};
--- models/rent.js
+++ models/rent.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const rentSchema = mongoose.Schema({
-	memberId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Member',
-		require: true,
-	},
-	bookId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Book",
-		require: true,
-	},
-	rentDate: {
-		type: Date,
-		default: Date.now()
-	},
-	returnDate: {
-		type: Date,
-		require: false,
-	}
-})
+  memberId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Member",
+    require: true
+  },
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    require: true
+  },
+  rentDate: {
+    type: Date,
+    default: Date.now()
+  },
+  returnDate: {
+    type: Date,
+    require: false
+  }
+});
 
-module.exports = mongoose.model('Rent', rentSchema);
+module.exports = mongoose.model("Rent", rentSchema);
