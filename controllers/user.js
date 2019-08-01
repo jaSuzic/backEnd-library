ac5f32e5b1be102910d1b97dfcc725fb9adcc297
Jaksa Suzic <jaksas@gmail.com>
2019-08-01 22:33:01 +0200

    Uradjen update usera

--- controllers/user.js
+++ controllers/user.js
@@ -12,13 +12,13 @@ exports.createNewUser = (req, res, next) => {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       position: req.body.position,
-      image: req.file ? (url + "/images/" + req.file.filename) : null
+      image: req.file ? url + "/images/" + req.file.filename : null
     });
     user
       .save()
       .then(result => {
         res.status(201).json({
-          message: "User created",
+          message: "User created"
         });
       })
       .catch(err => {
@@ -48,11 +48,13 @@ exports.loginUser = (req, res, next) => {
           message: "Auth failed"
         });
       // JWT is used now
-      const token = jwt.sign({
-        email: fetchedUser.email,
-        userId: fetchedUser._id
-      },
-        "Jaksa_Suzic_App_Library_secret_key", {
+      const token = jwt.sign(
+        {
+          email: fetchedUser.email,
+          userId: fetchedUser._id
+        },
+        "Jaksa_Suzic_App_Library_secret_key",
+        {
           expiresIn: "8h"
         }
       );
@@ -105,58 +107,86 @@ exports.updatePassword = (req, res, next) => {
       return bcrypt.compare(req.body.oldPass, user.password);
     })
     .then(result => {
-      if (!result) return res.status(401).json({
-        message: "Password not correct!"
-      });
+      if (!result)
+        return res.status(401).json({
+          message: "Password not correct!"
+        });
       bcrypt.hash(req.body.newPass, 10).then(hash => {
-        User.updateOne({
-          _id: fetchedUser._id
-        }, {
+        User.updateOne(
+          {
+            _id: fetchedUser._id
+          },
+          {
             password: hash
-          }).then(result => {
-            if (result.n > 0) {
-              res.status(200).json({
-                message: "Updated successful"
-              });
-            } else {
-              res.status(401).json({
-                message: "Update failed"
-              });
-            }
-          });;
+          }
+        ).then(result => {
+          if (result.n > 0) {
+            res.status(200).json({
+              message: "Updated successful"
+            });
+          } else {
+            res.status(401).json({
+              message: "Update failed"
+            });
+          }
+        });
       });
     });
 };
 
 exports.updateImage = (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host');
+  const url = req.protocol + "://" + req.get("host");
   //omoguciti ulogovanom useru da promeni SVOJU sliku
-  User.updateOne({
-    _id: req.body.id
-  }, {
-      image: url + '/images/' + req.file.filename
-    }).
-    then(result => {
-      if (result.n > 0) {
-        res.status(200).json({
-          message: 'Image updated!'
-        })
-      } else {
-        res.status(401).json({
-          message: "Update failed"
-        })
-      }
-    })
+  User.updateOne(
+    {
+      _id: req.body.id
+    },
+    {
+      image: url + "/images/" + req.file.filename
+    }
+  ).then(result => {
+    if (result.n > 0) {
+      res.status(200).json({
+        message: "Image updated!"
+      });
+    } else {
+      res.status(401).json({
+        message: "Update failed"
+      });
+    }
+  });
 };
 
 exports.getUsersExcept = (req, res, next) => {
   User.find({ $nor: [{ _id: req.body.id }] })
-    .select('-password')
+    .select("-password")
     .then(users => {
       if (users) {
         res.status(200).json(users);
       } else {
-        res.status(404).json({ message: "Error." })
+        res.status(404).json({ message: "Error." });
       }
-    })
-}
+    });
+};
+
+exports.updateUser = (req, res, next) => {
+  const url = req.protocol + "://" + req.get("host");
+  const updatedUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    position: req.body.position,
+    image: req.file ? url + "/images/" + req.file.filename : null
+  };
+  User.updateOne({ _id: req.body.id }, updatedUser).then(result => {
+    if (result.n > 0) {
+      res.status(200).json({
+        message: "Updated successful"
+      });
+    } else {
+      res.status(401).json({
+        message: "Update failed"
+      });
+    }
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/user");
 const router = express.Router();
-const multer = require('../middleware/multer')
+const multer = require("../middleware/multer");
 
-router.post('/login', userController.loginUser);
-router.post('/register', multer, userController.createNewUser);
-router.post('/updatePass', userController.updatePassword);
-router.patch('/updateImage', multer, userController.updateImage);
-router.post('/getUsers', userController.getUsersExcept);
+router.post("/login", userController.loginUser);
+router.post("/register", multer, userController.createNewUser);
+router.post("/updatePass", userController.updatePassword);
+router.patch("/updateImage", multer, userController.updateImage);
+router.post("/getUsers", userController.getUsersExcept);
+router.put("/updateUser", multer, userController.updateUser);
 
-module.exports = router; 
+module.exports = router;
